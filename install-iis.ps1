090ac94364637366d070d0c30f11fa8a7b835049
lerkiat <lerkiat@gmail.com>
2024-06-09 19:44:55 +0700

    Update install-iis.ps1

--- install-iis.ps1
+++ install-iis.ps1
@@ -1,3 +1,3 @@
 powershell.exe Install-WindowsFeature -name Web-Server -IncludeManagementTools
 powershell.exe Remove-Item -Path 'C:\inetpub\wwwroot\iisstart.htm'
-powershell.exe Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value "<center><h1><b>Welcome to $($env:computername) </b></h1></center>"
+powershell.exe Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value $($env:computername)
